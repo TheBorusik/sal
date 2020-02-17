09eab25558555b6f772e26c1b9dc3bfa4df1eabb
poowell <poowell@pipindl.ru>
2020-02-17 15:55:42 +0300

    test2

--- build.cmd
+++
@@ -1,5 +0,0 @@
-dotnet build --configuration Debug
-dotnet pack ./SAL.API/SAL.API.csproj  --no-build --output nupkgs
-dotnet pack ./SAL.Core/SAL.Core.csproj  --no-build --output nupkgs
-dotnet pack ./SAL.Infrastructure/SAL.Infrastructure.csproj  --no-build --output nupkgs
- 
--- update.cmd
+++
@@ -1,1 +0,0 @@
-@powershell -ExecutionPolicy Bypass -File ./updateVersion.ps1
