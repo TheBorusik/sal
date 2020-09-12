ab1b85b6c9a4ef7d1acee9a2520601faee9c7805
Куклев Павел <poowell@pipindl.ru>
2020-09-12 21:11:53 +0300

    build fvix

--- build.ps1
+++ build.ps1
@@ -23,12 +23,19 @@ Get-ChildItem -Filter *.csproj -Recurse | %{
 		}
 	}
 }
+<# 
+
+-p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+
+--include-symbols
+#>
+
 
 dotnet build --configuration $configurationName;
 if($LastExitCode -ne 0) { Write-Error 'Ошибка сборки' -ErrorAction Stop }
-dotnet pack ./SAL.API/SAL.API.csproj  --no-build --output nupkgs -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack ./SAL.API/SAL.API.csproj  --no-build --output nupkgs --include-symbols
 if($LastExitCode -ne 0) { Write-Error 'Ошибка упаковки SAL.API' -ErrorAction Stop }
-dotnet pack ./SAL.Core/SAL.Core.csproj  --no-build --output nupkgs -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack ./SAL.Core/SAL.Core.csproj  --no-build --output nupkgs --include-symbols
 if($LastExitCode -ne 0) { Write-Error 'Ошибка упаковки SAL.Core' -ErrorAction Stop }
-dotnet pack ./SAL.Infrastructure/SAL.Infrastructure.csproj  --no-build --output nupkgs -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack ./SAL.Infrastructure/SAL.Infrastructure.csproj  --no-build --output nupkgs --include-symbols
 if($LastExitCode -ne 0) { Write-Error 'Ошибка упаковки SAL.Infrastructure' -ErrorAction Stop }
