99d777d04ec593c98dfdf264472007dd51679bf6
Куклев Павел <poowell@pipindl.ru>
2020-10-21 12:04:56 +0300

    add FrontBaseExecuteHandler

---
+++ SAL.API/HandlersInterfaces/FrontCommand/FrontBaseExecuteHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using SAL.Infrastructure;
+
+namespace SAL.API.FrontCommand
+{
+    public abstract class FrontBaseExecuteHandler<TCommand, TCommandResult> : IFrontCommandHandlerAsync<TCommand, TCommandResult>
+        where TCommand : class, IHaveResult<TCommandResult>, new()
+        where TCommandResult : class, ICommandResult, new()
+    {
+        public abstract Task Handle(TCommand command);
+        
+        protected CommandContext commandContext;
+        protected ExecutingContext executingContext;
+        
+
+        public void SetContexts(CommandContext commandContext, ExecutingContext executingContext)
+        {
+            this.commandContext = commandContext;
+            this.executingContext = executingContext;
+
+        }
+        
+        protected Task PublishResult(TCommandResult result)
+        {
+            return executingContext.SalClient.PublishResultAsync(result, commandContext.Descriptor);
+        }
+
+        protected Task PublishResult(InternalExceptionDTO error)
+        {
+            return executingContext.SalClient.PublishResultAsync(error, commandContext.Descriptor);
+        }
+
+        protected Task PublishError(
+            string code,
+            string message = null,
+            object properties = null,
+            Exception innerException = null)
+        {
+            return PublishResult(SalError.CreateDto(code, message, properties, innerException));
+        }
+    }
+}
