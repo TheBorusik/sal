833e5cfb6e6bb1fddab57cc1b9bd1b320795dbd4
Куклев Павел <poowell@pipindl.ru>
2020-11-10 14:26:58 +0300

    fix Names

--- SAL.API/HandlersInterfaces/FrontCommand/ExternalHttp/ExternalHttpResponse.cs
+++ SAL.API/HandlersInterfaces/FrontCommand/ExternalHttp/ExternalHttpResponse.cs
@@ -4,8 +4,9 @@ namespace SAL.API.FrontCommand
 {
     public class ExternalHttpResponse
     {
-        public int HttpCode { get; set; }
+        public int StatusCode { get; set; }
         public byte[] Body { get; set; }
         public JObject Headers { get; set; }
+        public string ContentType { get; set; }
     }
 }
