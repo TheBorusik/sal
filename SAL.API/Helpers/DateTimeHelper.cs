8cb7652e3df90c6394e627effa2f65cf4c31f4ee
Куклев Павел <poowell@pipindl.ru>
2021-08-04 15:32:07 +0300

    add DateTimeHelper

---
+++ SAL.API/Helpers/DateTimeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SAL.API
+{
+    public static class DateTimeHelper
+    {
+        public static DateTime? ToUtc(this DateTime? value)
+        {
+            if (!value.HasValue) return null;
+            return value.Value switch
+            {
+                { Kind: DateTimeKind.Unspecified } => DateTime.SpecifyKind(value.Value, DateTimeKind.Utc),
+                { Kind: DateTimeKind.Utc } => value,
+                { Kind: DateTimeKind.Local } => value.Value.ToUniversalTime(),
+                _ => null
+            };
+        }
+        
+
+    }
+}
