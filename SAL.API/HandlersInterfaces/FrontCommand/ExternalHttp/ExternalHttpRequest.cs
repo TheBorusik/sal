13e4847e9fe6746f7fed8c93e81bb1f11d476c6c
Куклев Павел <poowell@pipindl.ru>
2021-09-11 18:30:57 +0300

    ExternalHttpRequest add host

--- SAL.API/HandlersInterfaces/FrontCommand/ExternalHttp/ExternalHttpRequest.cs
+++ SAL.API/HandlersInterfaces/FrontCommand/ExternalHttp/ExternalHttpRequest.cs
@@ -5,6 +5,7 @@ namespace SAL.API
     public class ExternalHttpRequest
     {
         public string Path { get; set; }
+        public string Host { get; set; }
         public string Method { get; set; }
         public string ContentType { get; set; }
         public JObject Headers { get; set; }
