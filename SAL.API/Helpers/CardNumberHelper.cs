dafe90075fee05a7c7df6750c6d1962e89575180
ikirillov <1992kir1992@mail.ru>
2022-03-30 12:17:27 +0300

    Увше MaskNumber

--- SAL.API/Helpers/CardNumberHelper.cs
+++ SAL.API/Helpers/CardNumberHelper.cs
@@ -6,27 +6,30 @@ using Newtonsoft.Json.Linq;
 
 namespace SAL.API
 {
-    public static class CardNumberHelper
+	public static class CardNumberHelper
     {
         public static char MaskChar = '*';
 
-        public static string MaskNumber(this string number)
+        public static string MaskNumber(this string number, int partLength = 0)
         {
             if (string.IsNullOrWhiteSpace(number))
                 return number;
-            
+
             var noSpace = Regex.Replace(number, @"\s+", "");
-            
-            if(!Regex.IsMatch(noSpace, @"^\d{13,19}$"))
+
+            if (!Regex.IsMatch(noSpace, @"^\d{13,19}$"))
                 return number;
 
             var mnumber = Regex.Replace(noSpace, @"(\d{6})(\d{6,9})(\d{4})",
                 m => m.Groups[1].Value + Repeat(MaskChar, m.Groups[2].Value.Length) + m.Groups[3].Value, RegexOptions.IgnoreCase);
 
-            var parts = mnumber.SplitInParts(4);
+            if (partLength < 1)
+                return mnumber;
+
+            var parts = mnumber.SplitInParts(partLength);
             return string.Join(" ", parts);
         }
-        
+
         public static string MaskNumber(this JValue value)
         {
             return MaskNumber(value.Value != null ? value.Value.ToString() : "");
@@ -39,8 +42,13 @@ namespace SAL.API
         {
             if (s == null)
                 throw new ArgumentNullException(nameof(s));
-            if (partLength <= 0)
+            if (partLength < 0)
                 throw new ArgumentException("Part length has to be positive.", nameof(partLength));
+            if (partLength == 0)
+            {
+                yield return s;
+                yield break;
+            }
 
             for (var i = 0; i < s.Length; i += partLength)
                 yield return s.Substring(i, Math.Min(partLength, s.Length - i));
